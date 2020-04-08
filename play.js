4f306ffa6dc42d57f5cca44a9d5486eb8beb3a70
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-08 21:27:56 +0000

    stdin input functions added to an input.js file

--- play.js
+++ play.js
@@ -1,26 +1,8 @@
 const net = require('net');
 const { connect } = require('./client');
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  })
-  return stdin;
-}
-
-const handleUserInput = (input) => {
-  if (input === '\u0003') {
-    process.exit();
-  }
-}
-
-setupInput();
-
-
+const { setupInput } = require('./input');
 
 console.log("Connecting...");
 connect();
+
+setupInput();
