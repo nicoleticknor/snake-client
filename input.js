ca62117efa18607232ab91dabb4da32a0f5180b2
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-08 21:48:38 +0000

    adding movement and 'say' functionality

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,8 +13,37 @@ const setupInput = function () {
 }
 
 const handleUserInput = (input) => {
+  let conn = connection;
   if (input === '\u0003') {
     process.exit();
+  }
+  if (input === '\u0077') {
+    console.log('up');
+    conn.write('Move: up');
+  }
+  if (input === '\u0073') {
+    console.log('down');
+    conn.write('Move: down');
+  }
+  if (input === '\u0064') {
+    console.log('right');
+    conn.write('Move: right');
+  }
+  if (input === '\u0061') {
+    console.log('left');
+    conn.write('Move: left');
+  }
+  if (input === '\u0066') {
+    conn.write('Say: WHAT IS LOVE')
+  }
+  if (input === '\u0067') {
+    conn.write("Say: BABY DON'T HURT ME")
+  }
+  if (input === '\u0063') {
+    conn.write("Say: DON'T HURT ME")
+  }
+  if (input === '\u0076') {
+    conn.write("Say: NO MORE")
   }
 }
 
--- play.js
+++ play.js
@@ -3,6 +3,6 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log("Connecting...");
-connect();
+const connection = connect();
 
-setupInput();
+setupInput(connection);
